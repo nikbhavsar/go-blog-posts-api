3dcba3c491457cdf0a45986c101fa040fb4f7cf3
Nikhar Bhavsar <bhavsarnikhar21@gmail.com>
2023-08-22 20:56:56 -0700

    DatabaseSetup

--- main.go
+++ main.go
@@ -4,13 +4,20 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/nikbhavsar/go-blog-posts-api/initalizers"
 )
 
+func init() {
+	initalizers.LoadEnvVariables()
+	initalizers.ConnectToDB()
+}
+
 func main() {
+
 	router := gin.Default()
 	router.GET("/", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
-			"message": "pong",
+			"message": "poyng",
 		})
 	})
 	router.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
---
+++ migrate/migrate.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"github.com/nikbhavsar/go-blog-posts-api/initalizers"
+	"github.com/nikbhavsar/go-blog-posts-api/models"
+)
+
+func init() {
+	initalizers.LoadEnvVariables()
+	initalizers.ConnectToDB()
+}
+
+func main() {
+	initalizers.DB.AutoMigrate(&models.Post{})
+}
